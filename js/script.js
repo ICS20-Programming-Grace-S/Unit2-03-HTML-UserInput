650b4c3f53f582b523d2bcf73ff936a98f55953e
Grace Silvaroli <99274228+GraceSilvaroli@users.noreply.github.com>
2022-03-23 15:06:31 +0000

    edit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Created by: Grace S
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function enterClicked () {
+  // this function gets user's first name and age and displays it back to user
+
+  // get first name from text field
+  let streetName = document.getElementById("street-name").value
+	// get age from text field and cast it to integer
+  let streetNumber = parseInt(document.getElementById("street-number").value)
+
+  // display name and age back to user
+  document.getElementById('user-info').innerHTML = "Your street name is " + streetName + " and your house number " + streetNumber + "
+}
